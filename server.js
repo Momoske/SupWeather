70b78340a08bd32672ea0f1e275eefb6a66f6547
Oscar Di Lenarda <oscar@dilenarda.com>
2021-04-05 18:18:02 +0200

    Fixed a bug when the server crashes

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ require('dotenv').config({path: './config.env'});
 
 const cors = require('cors');
 const express = require('express');
-const bodyParser = require('body-parser');
 const mongoSanitize = require('express-mongo-sanitize');
 
 const connectDB = require('./config/db');
@@ -16,8 +15,8 @@ connectDB();
 
 
 // middleware
-var corsOpts = {
-  origin: ['http://localhost:3000', 'https://supweather.netlify.app'],
+const corsOpts = {
+  origin: [process.env.NODE_ENV !== 'production' ? 'http://localhost:3000' : 'https://supweather.netlify.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 };
@@ -25,7 +24,6 @@ var corsOpts = {
 app.use(cors(corsOpts));
 app.use(express.json());
 app.use(mongoSanitize());
-app.use(bodyParser.urlencoded({extended:true}));
 
 app.use('/api/v1/auth', require('./routes/auth'));
 app.use('/api/v1/user', require('./routes/user'));
@@ -35,7 +33,7 @@ app.use(errorHandler); // needs to be last middleware used here
 
 
 // api endpoints
-app.get('/api/v1', (req, res) => res.status(200).send('Welcome to SupWeather!'));
+app.get('/', (req, res) => res.status(200).send('Welcome to SupWeather!'));
 
 
 // listener
