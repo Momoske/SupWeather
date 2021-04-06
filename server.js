2c03b1bc144254e6dba46ec24b309d73a1f2f389
Oscar Di Lenarda <oscar@dilenarda.com>
2021-04-06 19:35:22 +0200

    Added https support for development

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 require('dotenv').config({path: './config.env'});
 
+const fs = require('fs');
 const cors = require('cors');
+const https = require('https');
 const express = require('express');
 const mongoSanitize = require('express-mongo-sanitize');
 
 const connectDB = require('./config/db');
 const errorHandler = require('./middleware/error');
+
+
+// https config
+const cert = fs.readFileSync('cert.pem', 'utf8');
+const key  = fs.readFileSync('key.pem', 'utf8');
+const credentials = {key: key, cert: cert};
 
 
 // app & db config
@@ -16,7 +24,7 @@ connectDB();
 
 // middleware
 const corsOpts = {
-  origin: [process.env.NODE_ENV !== 'production' ? 'http://localhost:3000' : 'https://supweather.netlify.app'],
+  origin: [process.env.NODE_ENV !== 'production' ? 'https://localhost:3000' : 'https://supweather.netlify.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 };
@@ -37,7 +45,8 @@ app.get('/', (req, res) => res.status(200).send('Welcome to SupWeather!'));
 
 
 // listener
-const server = app.listen(port, () => console.log('Listening on port ' + port));
+const httpsServer = https.createServer(credentials, app);
+const server = httpsServer.listen(port, () => console.log('Listening on port ' + port));
 
 process.on('unhandledRejection', (error, _) => {
   console.log('Logged Error: '+error);
